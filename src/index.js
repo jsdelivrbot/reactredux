009051059379fe10462999f8609767b89166f269
Benjamin Hornisberger <benjamin.hornisberger@protonmail.com>
2018-08-09 00:24:02 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = "AIzaSyA94Kexu7rSxvx2OaT4NkzLALiMW5Pac3A";
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {videos:[]};
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+    render() {
+      return(
+        <div>
+          <SearchBar />
+          <VideoDetail video={this.state.videos[0]}/>
+          <VideoList videos={this.state.videos}/>
+        </div>
+      )
+    }
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector(".container"));
