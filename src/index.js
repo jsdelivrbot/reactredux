106b4ac21abbc75da4ff20bed927e07768030f67
Benjamin Hornisberger <benjamin.hornisberger@sts.ch>
2018-08-09 15:50:09 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -9,18 +9,27 @@ const API_KEY = "AIzaSyA94Kexu7rSxvx2OaT4NkzLALiMW5Pac3A";
 class App extends Component{
   constructor(props){
     super(props);
-    this.state = {videos:[]};
+    this.state = {
+      selectedVideo : null,
+      videos:[]
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo : videos[0]  
+      });
     });
   }
+
     render() {
       return(
         <div>
           <SearchBar />
-          <VideoDetail video={this.state.videos[0]}/>
-          <VideoList videos={this.state.videos}/>
+          <VideoDetail  video={this.state.selectedVideo}/>
+          <VideoList 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos}/>
         </div>
       )
     }
